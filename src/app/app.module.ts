5d4cb03f27d1e81a6f91596b2e0789ff35f42883
nn90 <41308435+nn90@users.noreply.github.com>
2018-08-12 12:59:43 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { CountryListComponent } from './country-list/country-list.component';
+import { CountryDetailsComponent } from './country-details/country-details.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'CountryList', component: CountryListComponent },
+  { path:'', redirectTo:'/welcome',pathMatch:'full' },
+ 
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    CountryListComponent,
+    CountryDetailsComponent,
+    NotFoundComponent
   ],
+  //Declaring RouterModule
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
